5298c30795671d53738ecc7386fcb4c8107f7657
ghyeon12345 <rkgus8686@naver.com>
2026-08-25 16:27:19 +0900

    main

--- product/main.java
+++ product/main.java
@@ -6,7 +6,7 @@ import Service.ProductService;
 import Service.ProductServiceImpl;
 
 public class Main{
-
+    
     static Scanner sc = new Scanner(System.in);
     static List<Product> products = null;
     static ProductService productService = new ProductServiceImpl();
@@ -14,7 +14,22 @@ public class Main{
 
     public static void menu() {
         System.out.println("::::::::: 상품관리 :::::::::");
-        System.out.println("1. 상품");
+        System.out.println("1. 상품 목록");
+        System.out.println("2. 상품 조회");
+        System.out.println("3. 상품 등록");
+        System.out.println("4. 상품 수정");
+        System.out.println("5. 상품 삭제");
+        System.out.println("0. 프로그램 종료");
+        System.out.println(":::::: 번호 입력 : ");
+    }
+    
+    /**
+     * 상품 목록
+    */
+   public static void list() {
+        System.out.println("::::::::: 상품관리 :::::::::");
+        productList = productService.list();
+        
     }
 
 
